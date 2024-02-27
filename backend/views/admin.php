d4e12406901d6ee886712600c4894954d0b5d95e
Vadym Gmyria <admin@vcredit.kiev.ua>
2024-02-27 10:19:06 +0200

    feat create and save user login and api-key

--- backend/views/admin.php
+++ backend/views/admin.php
@@ -13,68 +13,28 @@
  */
 ?>
 
-<div class="wrap">
+<div class="wrap yespo-settings-page">
+    <h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+    <?php
+    if ( get_option( 'yespo_options' ) !== false ){
+        $options = get_option('yespo_options', array());
+        if(isset($options['yespo_username'])) $yespo_username = $options['yespo_username'];
+        if(isset($options['yespo_api_key'])) $yespo_api_key = $options['yespo_api_key'];
+    }
+    ?>
+    <form method="post" action="options.php">
+        <div class="field-group">
+            <label for="username"><?php echo __('Username','yespo') ?></label>
+            <input type="text" id="username" name="yespo_username" value="<?php echo isset($yespo_username) ? $yespo_username : ''; ?>" />
+        </div>
+        <div class="field-group">
+            <label for="api_key"><?php echo __('Api Key (Password)','yespo') ?></label>
+            <input type="text" id="api_key" name="yespo_api_key" value="<?php echo isset($yespo_api_key) ? $yespo_api_key : ''; ?>" />
+        </div>
 
-	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
+        <?php wp_nonce_field( 'yespo_plugin_settings_save', 'yespo_plugin_settings_nonce' ); ?>
 
-	<div id="tabs" class="settings-tab">
-		<ul>
-			<li><a href="#tabs-1"><?php esc_html_e( 'Settings', Y_TEXTDOMAIN ); ?></a></li>
-			<li><a href="#tabs-2"><?php esc_html_e( 'Settings 2', Y_TEXTDOMAIN ); ?></a></li>
-			<?php
-			?>
-			<li><a href="#tabs-3"><?php esc_html_e( 'Import/Export', Y_TEXTDOMAIN ); ?></a></li>
-			<?php
-			?>
-		</ul>
-		<?php
-		require_once plugin_dir_path( __FILE__ ) . 'settings.php';
-		require_once plugin_dir_path( __FILE__ ) . 'settings-2.php';
-		?>
-		<?php
-		?>
-		<div id="tabs-3" class="metabox-holder">
-			<div class="postbox">
-				<h3 class="hndle"><span><?php esc_html_e( 'Export Settings', Y_TEXTDOMAIN ); ?></span></h3>
-				<div class="inside">
-					<p><?php esc_html_e( 'Export the plugin\'s settings for this site as a .json file. This will allows you to easily import the configuration to another installation.', Y_TEXTDOMAIN ); ?></p>
-					<form method="post">
-						<p><input type="hidden" name="y_action" value="export_settings" /></p>
-						<p>
-							<?php wp_nonce_field( 'y_export_nonce', 'y_export_nonce' ); ?>
-							<?php submit_button( __( 'Export', Y_TEXTDOMAIN ), 'secondary', 'submit', false ); ?>
-						</p>
-					</form>
-				</div>
-			</div>
+        <input type="submit" class="button button-primary" value="<?php echo __( 'Authorize', 'yespo' ); ?>" />
+    </form>
 
-			<div class="postbox">
-				<h3 class="hndle"><span><?php esc_html_e( 'Import Settings', Y_TEXTDOMAIN ); ?></span></h3>
-				<div class="inside">
-					<p><?php esc_html_e( 'Import the plugin\'s settings from a .json file. This file can be retrieved by exporting the settings from another installation.', Y_TEXTDOMAIN ); ?></p>
-					<form method="post" enctype="multipart/form-data">
-						<p>
-							<input type="file" name="y_import_file"/>
-						</p>
-						<p>
-							<input type="hidden" name="y_action" value="import_settings" />
-							<?php wp_nonce_field( 'y_import_nonce', 'y_import_nonce' ); ?>
-							<?php submit_button( __( 'Import', Y_TEXTDOMAIN ), 'secondary', 'submit', false ); ?>
-						</p>
-					</form>
-				</div>
-			</div>
-		</div>
-		<?php
-		?>
-	</div>
-
-	<div class="right-column-settings-page metabox-holder">
-		<div class="postbox">
-			<h3 class="hndle"><span><?php esc_html_e( 'Yespo.', Y_TEXTDOMAIN ); ?></span></h3>
-			<div class="inside">
-				<a href="https://github.com/WPBP/WordPress-Plugin-Boilerplate-Powered"><img src="https://raw.githubusercontent.com/WPBP/boilerplate-assets/master/icon-256x256.png" alt=""></a>
-			</div>
-		</div>
-	</div>
 </div>
--- functions/functions.php
+++ functions/functions.php
@@ -18,3 +18,29 @@
 function y_get_settings() {
 	return apply_filters( 'y_get_settings', get_option( Y_TEXTDOMAIN . '-settings' ) );
 }
+
+function yespo_save_settings() {
+    if ( ! isset( $_POST['yespo_plugin_settings_nonce'] ) || ! wp_verify_nonce( $_POST['yespo_plugin_settings_nonce'], 'yespo_plugin_settings_save' ) ) {
+        return;
+    }
+
+    if ( ! current_user_can( 'manage_options' ) ) {
+        return;
+    }
+
+    $options['yespo_username'] = sanitize_text_field( $_POST['yespo_username'] );
+    $options['yespo_api_key'] = sanitize_text_field( $_POST['yespo_api_key'] );
+    update_option( 'yespo_options', $options );
+}
+
+add_action( 'admin_init', 'yespo_save_settings' );
+
+
+function forms_updated_option_callback(){
+    if(isset($_REQUEST['settings-updated']) ) {
+        if ($_REQUEST['page'] === 'yespo' ) {
+            echo '<div class="notice notice-success is-dismissible"><p>' . __("Settings successfully saved!","yespo") . '</p></div>';
+        }
+    }
+}
+add_action('admin_notices', 'forms_updated_option_callback');

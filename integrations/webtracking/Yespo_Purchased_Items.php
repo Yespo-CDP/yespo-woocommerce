a5bceb2a67cde5da1d29475a896f1c6dd4f9a77b
Vadym Gmyria <admin@vcredit.kiev.ua>
2024-10-07 13:07:47 +0300

    thank you page 2

--- integrations/webtracking/Yespo_Purchased_Items.php
+++ integrations/webtracking/Yespo_Purchased_Items.php
@@ -8,38 +8,42 @@ class Yespo_Purchased_Items extends Yespo_Web_Tracking_Abstract
 {
     public function get_data(){
         // TODO: Implement get_data() method.
-        $cart_items = [];
+        if ( isset( $_GET['order-received'] ) && ! empty( $_GET['order-received'] ) ) {
 
-        $args = array(
-            'post_type'      => 'shop_order', // Тип поста - замовлення
-            'post_status'    => 'wc-completed', // Статус замовлення
-            'posts_per_page' => 1,            // Тільки одне замовлення
-            'orderby'        => 'date',       // Сортування за датою
-            'order'          => 'DESC',       // Від найновішого
-        );
+            $cart_items = [];
 
-        $query = new WP_Query( $args );
+            $args = array(
+                'post_type' => 'shop_order', // Тип поста - замовлення
+                'post_status' => 'wc-completed', // Статус замовлення
+                'posts_per_page' => 1,            // Тільки одне замовлення
+                'orderby' => 'date',       // Сортування за датою
+                'order' => 'DESC',       // Від найновішого
+            );
 
-        if ( $query->have_posts() ) {
-            $query->the_post();
-            $order_id = get_the_ID(); // Отримуємо ID останнього замовлення
-            $order = wc_get_order( $order_id ); // Отримуємо об'єкт замовлення
+            $query = new WP_Query($args);
+
+            if ($query->have_posts()) {
+                $query->the_post();
+                $order_id = get_the_ID(); // Отримуємо ID останнього замовлення
+                $order = wc_get_order($order_id); // Отримуємо об'єкт замовлення
 
 
-            $file_path = $_SERVER['DOCUMENT_ROOT'] . '/filedebug.txt';
-            $data_to_append = json_encode($order) . "\n";
-            $file_handle = fopen($file_path, 'a');
-            if ($file_handle) {
-                fwrite($file_handle, $data_to_append);
-                fclose($file_handle);
+                $file_path = $_SERVER['DOCUMENT_ROOT'] . '/filedebug.txt';
+                $data_to_append = json_encode($order) . "\n";
+                $file_handle = fopen($file_path, 'a');
+                if ($file_handle) {
+                    fwrite($file_handle, $data_to_append);
+                    fclose($file_handle);
+                }
+
+
+                wp_reset_postdata(); // Повертаємо оригінальний контекст
+
+                return $order; // Повертаємо об'єкт замовлення
             }
-
-
-
-            wp_reset_postdata(); // Повертаємо оригінальний контекст
-
-            return $order; // Повертаємо об'єкт замовлення
         }
+
+        return null;
 
         /*
         if ( class_exists( 'WooCommerce' ) && WC()->cart ) {

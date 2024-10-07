bd91cef9715cbb97fd6d59086f66269f9888205d
Vadym Gmyria <admin@vcredit.kiev.ua>
2024-10-07 16:22:33 +0300

    thank you page 3

--- integrations/webtracking/Yespo_Web_Tracking_Aggregator.php
+++ integrations/webtracking/Yespo_Web_Tracking_Aggregator.php
@@ -7,23 +7,27 @@ class Yespo_Web_Tracking_Aggregator
     private $category;
     private $product;
     private $cart;
+    private $purchase;
 
     public function __construct(){
         $this->category = new Yespo_Category_Event();
         $this->product = new Yespo_Product_Event();
         $this->cart = new Yespo_Cart_Event();
+        $this->purchase = new Yespo_Purchased_Event();
     }
 
     public function localize_scripts(){
 
         $category = $this->category->get_data();
         $product = $this->product->get_data();
+        $purchase = $this->purchase->get_data();
         //if( is_cart() ) $cart = $this->cart->get_data();
         //else $cart = null;
 
         $tracking_data = $this->get_localization_map(
             $category,
-            $product
+            $product,
+            $purchase
         );
 
         if (!empty($tracking_data)) {
@@ -34,7 +38,8 @@ class Yespo_Web_Tracking_Aggregator
 
     private function get_localization_map(
         $category,
-        $product
+        $product,
+        $purchase
     ){
 
         $tracking_data = [];
@@ -48,6 +53,14 @@ class Yespo_Web_Tracking_Aggregator
         }
 
         if (!is_null($product)) {
+            $tracking_data['product'] = array(
+                'id' => isset($product['id']) ? esc_js($product['id']) : '',
+                'price' => isset($product['price']) ? esc_js($product['price']) : '',
+                'stock' => isset($product['stock']) ? esc_js($product['stock']) : '',
+            );
+        }
+
+        if (!is_null($purchase)) {
             $tracking_data['product'] = array(
                 'id' => isset($product['id']) ? esc_js($product['id']) : '',
                 'price' => isset($product['price']) ? esc_js($product['price']) : '',
